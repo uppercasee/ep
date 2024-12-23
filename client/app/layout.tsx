7bd43d2009c54bd55c481977d220157635df5f6f
uppercasee <70530668+uppercasee@users.noreply.github.com>
2024-12-23 21:37:13 +0545

    sign in and sign up pages

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,34 +1,35 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css'
 import {
   ColorSchemeScript,
   MantineProvider,
   createTheme,
   mantineHtmlProps,
-} from "@mantine/core";
-import Navbar from "@/components/nav/navbar";
-import { ClerkProvider } from "@clerk/nextjs";
+} from '@mantine/core'
+import Navbar from '@/components/nav/navbar'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 export const metadata: Metadata = {
-  title: "Elearning Platform",
-  description: "A Gamified Elearning Platform using Nextjs. ",
-};
+  title: 'Elearning Platform',
+  description: 'A Gamified Elearning Platform using Nextjs. ',
+}
 
 const theme = createTheme({
-  fontFamily: "Verdana, sans-serif",
-  fontFamilyMonospace: "Monaco, Courier, monospace",
-  headings: { fontFamily: "Greycliff CF, sans-serif" },
+  fontFamily: 'Verdana, sans-serif',
+  fontFamilyMonospace: 'Monaco, Courier, monospace',
+  headings: { fontFamily: 'Greycliff CF, sans-serif' },
   // breakpoints: {
   //   md:'1024px'
   // }
-});
+})
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" {...mantineHtmlProps}>
@@ -36,13 +37,13 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <ClerkProvider>
-          <MantineProvider defaultColorScheme="dark" theme={theme}>
+        <MantineProvider defaultColorScheme="dark" theme={theme}>
+          <ClerkProvider appearance={{ baseTheme: dark }}>
             <Navbar />
             {children}
-          </MantineProvider>
-        </ClerkProvider>
+          </ClerkProvider>
+        </MantineProvider>
       </body>
     </html>
-  );
+  )
 }
