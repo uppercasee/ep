e6565e030489c8b12b200be5c7ad1bb80593ae73
uppercasee <70530668+uppercasee@users.noreply.github.com>
2025-01-05 18:23:17 +0545

    slugs and routes

---
+++ app/(dashboard)/courses/layout.tsx
@@ -0,0 +1,7 @@
+export default async function CourseLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <main className="">{children}</main>
+}
---
+++ app/(dashboard)/courses/new/page.tsx
@@ -0,0 +1,10 @@
+const NewCoursePage = () => {
+  return (
+    <>
+      <div>Create New Course (Only Accessible for Teachers)</div>
+      <div>Form</div>
+    </>
+  )
+}
+
+export default NewCoursePage
